ec4893b154a3f43e2e1b9789fad3493c5bf5f80f
cor1nthian <32943000+cor1nthian@users.noreply.github.com>
2024-05-05 02:38:44 +0300

    Create winremoveps1fromcontextmenu.ps1

---
+++ winremoveps1fromcontextmenu.ps1
@@ -0,0 +1,26 @@
+<#
+
+  .SYNOPSIS
+  Script removes ps1 script from explorer context menu
+  
+  .DESCRIPTION
+  Script removes ps1 script from explorer context menu
+
+  .INPUTS
+  None
+
+  .OUTPUTS
+  None
+  
+  .PARAMETERS
+  None
+  
+#>
+
+[System.String[]] $subKeys = @( 'Directory', 'Directory\Background', 'Drive' )
+
+foreach ($keyName in $subKeys) {
+    $path = "Registry::HKEY_CLASSES_ROOT\$keyName\shell\PowerShellCustomScript"
+
+    Remove-Item -Path $path -Force -Recurse -ErrorAction SilentlyContinue
+}
